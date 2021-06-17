47fd9db88497c5210faa51314064ce0618077d07
Elliott Slaughter <elliottslaughter@gmail.com>
2021-06-17 13:12:42 -0700

    Report sys.argv.

--- asdf/__main__.py
+++ asdf/__main__.py
@@ -1,4 +1,5 @@
 print("in asdf.__main__, __name__ is", __name__, "and __package__ is", __package__)
 
 import sys
+print("   and sys.args contains", repr(sys.argv))
 print("   and asdf is in sys.modules?", 'asdf' in sys.modules)
